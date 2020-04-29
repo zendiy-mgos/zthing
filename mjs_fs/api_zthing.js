faeb4a29517e6cc8112570baa138e55337f2ce36
Filippo Balestrini <filippo.balestrini@it.abb.com>
2020-04-29 12:46:40 +0200

    Improved JS

--- mjs_fs/api_zthing.js
+++ mjs_fs/api_zthing.js
@@ -1,6 +1,36 @@
 let ZenThing = {
   _free: ffi('void free(void *)'),
 
+  TYPE_SENSOR: 1,
+  TYPE_ACTUATOR: 3,
+
+  _onCreate: [],
+  // register on-create handler
+  _onCreateSub: function(f) {
+    this._onCreate.push(f);
+  },
+  // raise on-create event
+  _onCreatePub: function(t) {
+    for (let i = 0; i < this._onCreate.length; ++i) {
+    	this._onCreate[i](t);
+    }
+  },
+
+  _things: [],
+  // register a Zen Thingh
+  _regT: function(t) {
+    if (t === null || t === undefined) return false;
+    this._things.push(t);
+    return true;
+  },
+  _getTFromH: function(h) {
+    for (let i = 0; i < this._things.length; ++i) {
+      if (h === this._things[i].handle) {
+        return this._things[i];
+      }
+    }
+  },
+
   RESULT_ERROR: -1,
  
   // enum mgos_zthing_state_act
@@ -14,3 +44,7 @@ let ZenThing = {
   _proto: {
   },
 };
+
+ZenThing._onCreateSub(function(t) {
+  ZenThing._regT(t);
+});
