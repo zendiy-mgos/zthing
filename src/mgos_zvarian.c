7ce94bc7b71addc108f7aafa68cbb332b885244b
Filippo Balestrini <54440701+fbalestrini@users.noreply.github.com>
2020-10-30 14:55:38 +0100

    Update mgos_zvarian.c

--- src/mgos_zvarian.c
+++ src/mgos_zvarian.c
@@ -1,3 +1,76 @@
 #include "mgos_zvariant.h"
 
+bool mgos_zvariant_integer_set(struct mgos_zvariant *v, int value) {
+  if (!v) return false;
+  v->type = ZVARIANT_INT;
+  v->value.i = value;
+  return true;
+}
 
+bool mgos_zvariant_long_set(struct mgos_zvariant *v, long value) {
+  if (!v) return false;
+  v->type = ZVARIANT_LONG;
+  v->value.l = value;
+  return true;
+}
+
+bool mgos_zvariant_bool_set(struct mgos_zvariant *v, bool value) {
+  if (!v) return false;
+  v->type = ZVARIANT_BOOL;
+  v->value.b = value;
+  return true;
+}
+
+bool mgos_zvariant_double_set(struct mgos_zvariant *v, double value) {
+  if (!v) return false;
+  v->type = ZVARIANT_DOUBLE;
+  v->value.d = value;
+  return true;
+}
+
+bool mgos_zvariant_float_set(struct mgos_zvariant *v, float value) {
+  if (!v) return false;
+  v->type = ZVARIANT_FLOAT;
+  v->value.f = value;
+  return true;
+}
+
+bool mgos_zvariant_integer_get(struct mgos_zvariant *v, int *value) {
+  if (v && (v->type == ZVARIANT_INT) && value) {
+    *value = v->i;
+    reurn true;
+  }
+  return false;
+}
+
+bool mgos_zvariant_long_get(struct mgos_zvariant *v, long *value) {
+  if (v && (v->type == ZVARIANT_LONG) && value) {
+    *value = v->l;
+    reurn true;
+  }
+  return false;
+}
+
+bool mgos_zvariant_bool_get(struct mgos_zvariant *v, bool *value) {
+  if (v && (v->type == ZVARIANT_BOOL) && value) {
+    *value = v->b;
+    reurn true;
+  }
+  return false;
+}
+
+bool mgos_zvariant_double_get(struct mgos_zvariant *v, double *value) {
+  if (v && (v->type == ZVARIANT_DOUBLE) && value) {
+    *value = v->d;
+    reurn true;
+  }
+  return false;
+}
+
+bool mgos_zvariant_float_get(struct mgos_zvariant *v, float *value) {
+  if (v && (v->type == ZVARIANT_FLOAT) && value) {
+    *value = v->f;
+    reurn true;
+  }
+  return false;
+}
