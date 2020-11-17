cd8d87808337dd50e56251099be6ccef47ce364b
Filippo Balestrini <54440701+fbalestrini@users.noreply.github.com>
2020-11-17 15:25:48 +0100

    Update api_zvar.js

--- mjs_fs/api_zvar.js
+++ mjs_fs/api_zvar.js
@@ -1,12 +1,12 @@
 let ZenVar = {
   _tof: ffi('int mjs_zvariant_type_get(void *)'),
-  _bs: ffi('bool mgos_zvariant_bool_set(void *, bool)'),
+  _bs: ffi('void* mgos_zvariant_bool_set(void *, bool)'),
   _bg: ffi('bool mjs_zvariant_bool_get(void *)'),
-  _is: ffi('bool mgos_zvariant_integer_set(void *, int)'),
+  _is: ffi('void* mgos_zvariant_integer_set(void *, int)'),
   _ig: ffi('int mjs_zvariant_integer_get(void *)'),
-  _fs: ffi('bool mgos_zvariant_float_set(void *, float)'),
+  _fs: ffi('void* mgos_zvariant_float_set(void *, float)'),
   _fg: ffi('float mjs_zvariant_float_get(void *)'),
-  _ds: ffi('bool mgos_zvariant_double_set(void *, double)'),
+  _ds: ffi('void* mgos_zvariant_double_set(void *, double)'),
   _dg: ffi('double mjs_zvariant_double_get(void *)'),
   _isnav: ffi('bool mgos_zvariant_is_nav(void *)'),
   
