cc7be9dccb7392abda65114a3a6ecb3b472c5725
Filippo Balestrini <54440701+fbalestrini@users.noreply.github.com>
2020-11-25 15:43:11 +0100

    Update mgos_zthing.c

--- src/mgos_zthing.c
+++ src/mgos_zthing.c
@@ -8,21 +8,22 @@
 /* Excecution context instance */
 static struct mg_zthing_ctx *s_context = NULL;
 
-/* Handler's attachments context entry */
-struct mg_zthing_att_ctx_e {
+/* Handler's extensions context entry */
+struct mg_zthing_ext_ctx_e {
   struct mgos_zthing *handle;
-  void *att;
-  SLIST_ENTRY(mg_zthing_att_ctx_e) entry;
+  const char* name;
+  void *ext;
+  SLIST_ENTRY(mg_zthing_ext_ctx_e) entry;
 };
 
-/* Handler's attachments context */
-struct mg_zthing_atts_ctx {
+/* Handler's extensions context */
+struct mg_zthing_exts_ctx {
   int version;
-  SLIST_HEAD(entries, mg_zthing_att_ctx_e) entries;
+  SLIST_HEAD(entries, mg_zthing_ext_ctx_e) entries;
 };
 
-/* Handler's attachments context instance */
-static struct mg_zthing_atts_ctx *s_atts_context = NULL;
+/* Handler's extensions context instance */
+static struct mg_zthing_exts_ctx *s_exts_context = NULL;
 
 bool mgos_zthing_register(struct mgos_zthing *handle) {
   if (handle == NULL) return false;
@@ -45,13 +46,25 @@ void mgos_zthing_close(struct mgos_zthing *handle) {
   free(handle);
 }
 
-bool mgos_zthing_attach_ext(struct mgos_zthing *handle, void *ext) {
-  if (handle == NULL || ext == NULL) return false;
-  struct mg_zthing_att_ctx_e *e = calloc(1, sizeof(struct mg_zthing_att_ctx_e));
+bool mgos_zthing_ext_attach(struct mgos_zthing *handle, const char* ext_name, void *ext) {
+  if (handle == NULL || ext == NULL || !ext_name) return false;
+  struct mg_zthing_ext_ctx_e *e = calloc(1, sizeof(struct mg_zthing_ext_ctx_e));
+  e->handle = handle;
+  e->ext = ext;
+  e->name = ext_name;
   if (e) {
-    SLIST_INSERT_HEAD(&s_context->entries, e, entry);
+    SLIST_INSERT_HEAD(&s_exts_context->entries, e, entry);
   }
   return (e != NULL);
+}
+
+bool mgos_zthing_ext_get(struct mgos_zthing *handle, const char* ext_name, void *ext) {
+  if (handle == NULL || !ext_name) return NULL;
+  struct mg_zthing_ext_ctx_e *e;
+  SLIST_FOREACH(e, &s_exts_context->entries, entry) {
+    if ((((void *)e->handle) == ((void *)handle)) && (0 == strcasecmp(e->name, ext_name))) return e;
+  }
+  return NULL;
 }
 
 
@@ -68,13 +81,13 @@ bool mgos_zthing_init() {
   s_context->version = 1;
   SLIST_INIT(&s_context->handles);
   
-  /* Create handler's attachments context */
-  s_atts_context = calloc(1, sizeof(struct mg_zthing_atts_ctx));
-  if (!s_atts_context) return false;
+  /* Create handler's extensions context */
+  s_exts_context = calloc(1, sizeof(struct mg_zthing_exts_ctx));
+  if (!s_exts_context) return false;
   
-  /* Initialize handler's attachments context */
-  s_atts_context->version = 1;
-  SLIST_INIT(&s_atts_context->entries);
+  /* Initialize handler's extensions context */
+  s_exts_context->version = 1;
+  SLIST_INIT(&s_exts_context->entries);
 
   /* LOG(LL_INFO, ("MGOS_ZTHING_EVENT_BASE %d",
     MGOS_ZTHING_EVENT_BASE)); */
