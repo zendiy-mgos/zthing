493fe75a87f3960b8187803ca8ca2e2367a0c830
Filippo Balestrini <54440701+fbalestrini@users.noreply.github.com>
2020-11-25 15:28:50 +0100

    Update mgos_zthing.c

--- src/mgos_zthing.c
+++ src/mgos_zthing.c
@@ -8,6 +8,22 @@
 /* Excecution context instance */
 static struct mg_zthing_ctx *s_context = NULL;
 
+/* Handler's attachments context entry */
+struct mg_zthing_att_ctx_e {
+  struct mgos_zthing *handle;
+  void *att;
+  SLIST_ENTRY(mg_zthing_att_ctx_e) entry;
+};
+
+/* Handler's attachments context */
+struct mg_zthing_atts_ctx {
+  int version;
+  SLIST_HEAD(entries, mg_zthing_att_ctx_e) entries;
+};
+
+/* Handler's attachments context instance */
+static struct mg_zthing_atts_ctx *s_atts_context = NULL;
+
 bool mgos_zthing_register(struct mgos_zthing *handle) {
   if (handle == NULL) return false;
   SLIST_INSERT_HEAD(&s_context->handles, handle, entry);
@@ -29,8 +45,13 @@ void mgos_zthing_close(struct mgos_zthing *handle) {
   free(handle);
 }
 
-struct mg_zthing_ctx *mg_zthing_ctx_get() {
-  return s_context;
+bool mgos_zthing_attach_ext(struct mgos_zthing *handle, void *ext) {
+  if (handle == NULL || ext == NULL) return false;
+  struct mg_zthing_att_ctx_e *e = calloc(1, sizeof(struct mg_zthing_att_ctx_e));
+  if (e) {
+    SLIST_INSERT_HEAD(&s_context->entries, e, entry);
+  }
+  return (e != NULL);
 }
 
 
@@ -39,13 +60,21 @@ bool mgos_zthing_init() {
     return false;
   }
 
-  /* Create the context */
+  /* Create execution context */
   s_context = calloc(1, sizeof(struct mg_zthing_ctx));
   if (!s_context) return false;
   
-  /* Initialize the context */
+  /* Initialize execution context */
   s_context->version = 1;
   SLIST_INIT(&s_context->handles);
+  
+  /* Create handler's attachments context */
+  s_atts_context = calloc(1, sizeof(struct mg_zthing_atts_ctx));
+  if (!s_atts_context) return false;
+  
+  /* Initialize handler's attachments context */
+  s_atts_context->version = 1;
+  SLIST_INIT(&s_atts_context->entries);
 
   /* LOG(LL_INFO, ("MGOS_ZTHING_EVENT_BASE %d",
     MGOS_ZTHING_EVENT_BASE)); */
